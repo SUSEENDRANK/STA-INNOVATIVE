1dc0c3c55749309a23df68e5692c171a45b5870e
suseendran <suseendran04@gmail.com>
2026-05-06 11:27:38 +0530

    Add files via upload
    Tester copy the code to valid the testcase

---
+++ STA innovative/main.py
@@ -0,0 +1,43 @@
+# Login Authentication System with Errors
+
+print("====== LOGIN SYSTEM ======")
+
+username = input("Enter Username : ")
+password = input("Enter Password : ")
+
+saved_username = "admin"
+saved_password = "admin123"
+
+attempts = 3
+
+for i in range(attempts):
+
+    print("\nChecking Login Details...")
+
+    if username == saved_username:
+        print("Username Found")
+
+        # Error line
+        if password == saved_pass:
+
+            print("Password Correct")
+            print("Login Successful")
+
+            print("Welcome to Dashboard")
+            print("Loading User Data...")
+            print("Access Granted")
+
+            break
+
+        else:
+            print("Wrong Password")
+
+    else:
+        print("Invalid Username")
+
+    print("Attempts Left:", attempts - i - 1)
+
+    username = input("Re-enter Username : ")
+    password = input("Re-enter Password : ")
+
+print("\nSystem Closed")
